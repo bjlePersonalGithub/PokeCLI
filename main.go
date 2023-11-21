d62e3039d2d0e18685259cf773364feb045f436d
bjle <bjle.business@gmail.com>
2023-11-21 17:15:17 -0500

    Set up basic CLI with scanner and user input

--- main.go
+++ main.go
@@ -1,9 +1,60 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
-	fmt.Println("Hello, World")
+	for {
+		fmt.Print("Pokedex > ")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		if err := scanner.Err(); err != nil {
+			fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		}
+		userInput := scanner.Text()
+		commands := getCLICommands()
+
+		command, commandExist := commands[userInput]
+
+		if commandExist {
+			fmt.Println(command.name)
+		} else {
+			fmt.Println("Invalid Command")
+		}
+
+	}
+}
+
+func getCLICommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	// Your implementation here
+	return nil
+}
+
+func commandExit() error {
+	return errors.New("Exit Program")
 }
