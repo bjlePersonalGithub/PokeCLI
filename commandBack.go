0b3ff318c009b5fe1aef4e6686ec246697c6f6f2
bjle <bjle.business@gmail.com>
2023-12-17 20:05:13 -0500

    add guard clause

--- commandBack.go
+++ commandBack.go
@@ -1,32 +1,11 @@
 package main
 
+import "fmt"
+
 func commandMapBack() error {
-	// if mapState.prevURL == "" {
-	// 	fmt.Println("You're already at the first page")
-	// 	return nil
-	// }
-
-	// res, err := http.Get(mapState.prevURL)
-	// if err != nil {
-	// 	return err
-	// }
-	// defer res.Body.Close()
-
-	// if res.StatusCode != http.StatusOK {
-	// 	return fmt.Errorf("request failed with status code: %d", res.StatusCode)
-	// }
-
-	// var mapData mapData
-	// if err := json.NewDecoder(res.Body).Decode(&mapData); err != nil {
-	// 	return err
-	// }
-
-	// for _, result := range mapData.Results {
-	// 	fmt.Println(result.Name)
-	// }
-
-	// mapState.nextURL = mapState.prevURL // Store current URL as next
-	// mapState.prevURL = mapData.Previous // Update the prevURL for the previous fetch
+	if displayData.PreviousMapData == nil || len(displayData.PreviousMapData) == 0 {
+		return fmt.Errorf("no previous map data")
+	}
 
 	return nil
 }
